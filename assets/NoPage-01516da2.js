4f7f51ddc1e9c7d70ae3ce31690e568ea3f6a094
Indraja <A7321@axtria.com>
2024-09-03 15:15:21 +0530

    Updates

---
+++ assets/NoPage-01516da2.js
@@ -0,0 +1,2 @@
+import{a$ as o}from"./vendor-297620a5.js";function e(){return o.jsx("h1",{children:"404"})}e.displayName="NoPage";export{e as Component};
+//# sourceMappingURL=NoPage-01516da2.js.map
--- assets/NoPage-27729d29.js
+++
@@ -1,2 +0,0 @@
-import{a$ as o}from"./vendor-67812f1b.js";function e(){return o.jsx("h1",{children:"404"})}e.displayName="NoPage";export{e as Component};
-//# sourceMappingURL=NoPage-27729d29.js.map
